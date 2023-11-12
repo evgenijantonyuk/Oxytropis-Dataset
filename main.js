2feaa14a15ca9273d1e6563504e0c0cacaecca5d
Evgenij Antonyuk <96491514+evgenijantonyuk@users.noreply.github.com>
2023-11-12 12:24:14 +0700

    add gallery

--- main.js
+++ main.js
@@ -1,41 +1,36 @@
-// document.querySelector('#coworker').onclick = function () {
-//     alert('Clicked')
+
+new Gallery(document.getElementById('gallery'), {
+    margin: 15
+});
+
+// document.querySelector('#elastic').oninput = function () {
+//     let val = this.value.trim()
+//     let elasticItem = document.querySelectorAll('.species__name-lat')
+//     let speciesLiterature = document.querySelectorAll('.species-literature')
+//
+//     if (val != '') {
+//         elasticItem.forEach(function (elem) {
+//             if (elem.innerText.search(val) == -1) {
+//                 elem.classList.add('hide')
+//                 elem.innerHTML = elem.innerText
+//             }
+//             else {
+//                 elem.classList.remove('hide')
+//                 let str = elem.innerText
+//                 elem.innerHTML = insertMark(str, elem.innerText.search(val), val.length)
+//             }
+//         });
+//     }
+//     else {
+//         elasticItem.forEach(function (elem) {
+//             elem.classList.remove('hide')
+//             elem.innerHTML = elem.innerText
+//         });
+//     }
 // }
-
-// function inputLetter() {
-//     // document.querySelector('#coworker').innerHTML = 'Введите сообщение'
-//     document.querySelector('#coworker').onclick = function () {
-//     alert('Clicked')
+//
+// function insertMark(string, pos, len) {
+//     return string.slice(0, pos) + '<mark>' + string.slice(pos, pos + len) + '</mark>' + string.slice(pos + len)
 // }
-
-document.querySelector('#elastic').oninput = function () {
-    let val = this.value.trim()
-    let elasticItem = document.querySelectorAll('.species__name-lat')
-    let speciesLiterature = document.querySelectorAll('.species-literature')
-    
-    if (val != '') {
-        elasticItem.forEach(function (elem) {
-            if (elem.innerText.search(val) == -1) {
-                elem.classList.add('hide')
-                elem.innerHTML = elem.innerText
-            }
-            else {
-                elem.classList.remove('hide')
-                let str = elem.innerText
-                elem.innerHTML = insertMark(str, elem.innerText.search(val), val.length)
-            }
-        });
-    }
-    else {
-        elasticItem.forEach(function (elem) {
-            elem.classList.remove('hide')
-            elem.innerHTML = elem.innerText
-        });
-    }
-}
-
-function insertMark(string, pos, len) {
-    return string.slice(0, pos) + '<mark>' + string.slice(pos, pos + len) + '</mark>' + string.slice(pos + len)
-}
-
-
+//
+//
